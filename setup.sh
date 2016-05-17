3ceb11560d86f845d3e8b3a5d2861750d1b02d79
Ethan Estrada <ethan.estrada@gmail.com>
2016-05-17 12:23:16 -0600

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+function relpath()
+{
+    python -c "import os, sys; sys.stdout.write(os.path.relpath('${PWD}/${1}', '${HOME}'))";
+    # echo 1>&2
+}
+
+function linknew()
+{
+    mv -vf "${HOME}/${2}" "${HOME}/${2}.prev"
+    ln -vfs $(relpath ${1}) "${HOME}/${2}"
+}
+
+linknew  bash/bashrc.sh .bashrc
+linknew  bash/bash_logout.sh .bash_logout
+linknew  bash/bash_profile.sh .bash_profile
+linknew  emacs.el .emacs
+linknew  guile.scm .guile
+linknew  vimrc.vim .vimrc
+linknew  gitconfig.ini .gitconfig
+linknew  hgrc.ini .hgrc
+linknew  idlerc .idlerc
