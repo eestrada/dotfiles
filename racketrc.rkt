72c70541dfd23e8384ac5554a06296ff788386ac
Ethan Estrada <ethan.estrada@gmail.com>
2016-06-21 23:53:19 -0600

    Use "xrepl" readline library when using an interactive terminal

--- racketrc.rkt
+++ racketrc.rkt
@@ -1,6 +1,4 @@
-(require readline)
-
-;; FIXME: Use this snippet instead?
-;; (when (regexp-match? #rx"term"
-;;                      (getenv "TERM"))
-;;   (dynamic-require 'readline #f))
+;; NOTE: Only load xrepl readline support when in an interactive terminal
+(when (regexp-match? #rx"term"
+                     (getenv "TERM"))
+  (dynamic-require 'xrepl #f))
