bbad819e8229da106d17f2239032db91cb61ac34
Ethan Estrada <ethan.estrada@gmail.com>
2018-05-10 08:33:01 -0600

    Directly link to shell rc scripts

--- setup.sh
+++ setup.sh
@@ -15,6 +15,8 @@ linknew()
     ln -vfs $(relpath ${1} ${2}) "${HOME}/${2}"
 }
 
+linknew  bash/bashrc.sh .shrc
+linknew  bash/bashrc.sh .mkshrc
 linknew  bash/bashrc.sh .bashrc
 linknew  bash/bash_logout.sh .bash_logout
 linknew  bash/bash_profile.sh .bash_profile
@@ -33,12 +35,9 @@ linknew  i3/i3status.conf .i3status.conf
 linknew  i3 .i3
 linknew  xinitrc.sh .xinitrc
 linknew  Xmodmap .Xmodmap
-# linknew  Xdefaults .Xdefaults
 linknew  Xresources .Xresources
 linknew  Xresources.d .Xresources.d
 linknew  notestxtrc.sh .notestxtrc
-
-# ln -vs .mkshrc ~/.shrc
 
 # NOTE: nested folders under .config and similar
 # linknew  dunst .config/dunst  # FIXME: won't work because we can't do symlinks to nested folders yet
