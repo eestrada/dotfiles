c4b2f92d6d83b6c0081bc7bcc044a5d27efc20f9
Ethan Estrada <ethan.estrada@gmail.com>
2019-08-26 10:22:20 -0600

    Add polling xrandr "daemon"
    It isn't very useful since devd is working as expected now that I understand it
    better. However, there is still a few useful things in this script can could be
    generalized for use elsewhere.

---
+++ bin/xrandr_garbage_daemon.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# This daemon is garbage. Use it anyway because you don't have much choice since devd refuses to match your entry in devd.conf.
+
+# NOTE: we don't want to hog resources, so renice ourselves
+
+renice 20 $$
+
+hdmi1_state ()
+{
+    xrandr -q | grep HDMI1 | cut -w -f2
+}
+
+_prev=$(hdmi1_state)
+
+while sleep 2
+do
+    if [ $(hdmi1_state) '!=' ${_prev} ]; then
+        _prev=$(hdmi1_state)
+        xrandr -q >/dev/null
+        xrandr --output eDP1 --auto --pos 0x0 --output HDMI1 --auto --right-of eDP1
+    fi
+done
+
