83848d7f6dee70ad37b993573553d63599e8db7d
Ethan Estrada <ethan@misterfidget.com>
2020-03-24 10:39:34 -0600

    Make setup.sh script more intelligent about the system it installs to

--- setup.sh
+++ setup.sh
@@ -30,6 +30,7 @@ linknew()
     ln -vfs "$(relpath ${1} ${2})" "$(joinpath ${HOME} ${2})"
 }
 
+echo "Setting up configs general to all systems."
 linknew  sh/shrc.sh .shrc
 linknew  sh/shrc.sh .mkshrc
 linknew  sh/shrc.sh .bashrc
@@ -49,17 +50,32 @@ linknew  gitconfig.ini .gitconfig
 linknew  hgrc.ini .hgrc
 linknew  tmux.conf .tmux.conf
 linknew  color-themes/base16-shell .base16-shell
-# linknew  xscreensaver .xscreensaver
-# linknew  i3/i3status.conf .i3status.conf
-# linknew  i3 .i3
-# linknew  xinitrc.sh .xinitrc
-# linknew  Xmodmap .Xmodmap
-# linknew  Xresources .Xresources
-# linknew  Xresources.d .Xresources.d
 linknew  notestxtrc.sh .notestxtrc
 
-# NOTE: nested folders under .config and similar
-# linknew  dunst .config/dunst  # FIXME: won't work because we can't do symlinks to nested folders yet
+if [ $(uname -s) = "Darwin" ]; then
+  echo "\nSetting up macOS/Darwin specific configs:"
+  linknew  macOS/hammerspoon .hammerspoon
+elif [ $(uname -s) = "FreeBSD" ] || [ $(uname -s) = "Linux" ]; then
+  echo "\nSetting up configs general to both FreeBSD and Linux:"
+  linknew  xscreensaver .xscreensaver
+  linknew  i3/i3status.conf .i3status.conf
+  linknew  i3 .i3
+  linknew  xinitrc.sh .xinitrc
+  linknew  Xmodmap .Xmodmap
+  linknew  Xresources .Xresources
+  linknew  Xresources.d .Xresources.d
 
-# mkdir -p ~/.vdirsyncer
-# linknew  vdirsyncer/config.ini ~/.vdirsyncer/config
+  # NOTE: nested folders under .config and similar
+  # linknew  dunst .config/dunst  # FIXME: won't work because we can't do symlinks from nested folders yet
+
+  # mkdir -p ~/.vdirsyncer
+  # linknew  vdirsyncer/config.ini ~/.vdirsyncer/config
+
+  if [ $(uname -s) = "FreeBSD" ]; then
+    echo "\nSetting up configs specific to FreeBSD:"
+  elif [ $(uname -s) = "Linux" ]; then
+    echo "\nSetting up configs specific to Linux:"
+  fi
+else
+  echo "\nNot on a system we recognize as needing special configs: $(uname -s)"
+fi
