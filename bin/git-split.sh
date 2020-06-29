11e4515d127086e306f585d24b289a4d63e4acfd
Ethan Estrada <ethan@misterfidget.com>
2020-06-29 17:41:37 -0600

    Make commit messages easier to read from git-split.sh

--- bin/git-split.sh
+++ bin/git-split.sh
@@ -11,18 +11,18 @@ git_pwd="$(pwd)"
 
 git mv "$1" "$2"
 cd "$git_pwd"
-git commit -n -m "Split $1 to $2 - rename to target"
+git commit -n -m "Split '$1' to '$2' - rename to target"
 REV=`git rev-parse HEAD`
 
 git reset --hard HEAD^
 cd "$git_pwd"
 git mv "$1" "${1}_temp"
 cd "$git_pwd"
-git commit -n -m "Split $1 to $2 - rename to temp"
+git commit -n -m "Split '$1' to '$2' - rename to temp"
 
 git merge $REV
-git commit -a -n -m "Split $1 to $2 - keep both files"
+git commit -a -n -m "Split '$1' to '$2' - keep both files"
 
 git mv "${1}_temp" "$1"
 cd "$git_pwd"
-git commit -n -m "Split $1 to $2 - restore source from temp"
+git commit -n -m "Split '$1' to '$2' - restore source from temp"
