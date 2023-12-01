7655def92bd0b6afa2d1be16bcdd583bc5385b77
Ethan Estrada <ethan@misterfidget.com>
2023-11-30 18:25:43 -0600

    Add listchars to neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,15 +4,13 @@ vim.opt.uc = 0
 -- Syntax settings
 vim.opt.background = 'dark'
 vim.cmd('syntax enable') -- Make sure syntax highlighting is on for supported file types.
-vim.cmd('colorscheme slate')
+vim.cmd('colorscheme desert')
 vim.opt.list = true -- turn on special character highlighting
--- vim.opt.listchars=trail:·,tab:»-,eol:¬ -- define what special characters look like
+vim.opt.listchars = { trail = '·', tab = '»-', eol = '¬' } -- define what special characters look like
 
 vim.opt.autoindent = true -- Copy indent from current line when starting a new line.
 vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent.
-
 vim.opt.errorbells = false -- Do not ring the bell for error messages.
-
 vim.opt.visualbell = true -- Use visual bell instead of beeping.
 
 -- set wrap -- Lines longer than the width of the window will wrap.
@@ -24,7 +22,7 @@ vim.opt.number = true -- Print the line number in front of each line.
 vim.opt.numberwidth = 4 -- Minimal number of columns to use for the line number.
 
 -- Does not work in lua in this form
--- vim.opt.backspace = 2 -- make backspace work line most other apps
+-- vim.opt.backspace = 2 -- make backspace work like most other apps
 
 -- Tab Settings
 -- set expandtab -- Use the appropriate number of spaces to insert a	<Tab>.
@@ -32,6 +30,7 @@ vim.opt.expandtab = false -- Messes up Makefiles and stuff. Just hit spacebar a 
 vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while editing.
 vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for.
 
+-- Will need to be refactored to work in Lua
 -- -- System friendly settings
 -- -- Using system clipboard pulled from: http://vimcasts.org/episodes/accessing-the-system-clipboard-from-vim/
 -- -- Use system clipboard for Yank and Put
@@ -41,6 +40,7 @@ vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for.
 --     set clipboard=unnamed,unnamedplus
 -- endif
 
+-- Will need to be refactored to work in Lua
 -- Delete Trailing whitespace
 --  Autocmd pulled from:
 --  http://vim.wikia.com/wiki/Remove_unwanted_spaces#Automatically_removing_all_trailing_whitespace
