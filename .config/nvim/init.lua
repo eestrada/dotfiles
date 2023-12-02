50369200e93fd9ca472b356a374b75542f115433
Ethan Estrada <ethan@misterfidget.com>
2023-12-02 04:15:14 -0600

    Try to figure out neovim lsp configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,12 @@
 require('options')
+
+-- Only works if lsp plugin has been installed
+vim.cmd('packadd lspconfig')
+
+-- Based on info in link below:
+-- https://cs.opensource.google/go/x/tools/+/refs/tags/gopls/v0.14.2:gopls/doc/vim.md#neovim
+local lspconfig = require("lspconfig")
+lspconfig.gopls.setup({})
+
+-- lsp configuration suggestions can be found here:
+-- https://github.com/neovim/nvim-lspconfig/blob/master/README.md
