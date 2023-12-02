2bf3ceb60a908e4d2247290436584bad5dc34f18
Ethan Estrada <ethan@misterfidget.com>
2023-12-02 15:18:37 -0600

    Use autoloaded vim-plug to bootstrap all plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,17 @@
 require('options')
 
--- Only works if lsp plugin has been installed
-vim.cmd('packadd lspconfig')
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
+
+Plug('https://github.com/neovim/nvim-lspconfig')
+
+vim.call('plug#end')
 
 -- Based on info in link below:
 -- https://cs.opensource.google/go/x/tools/+/refs/tags/gopls/v0.14.2:gopls/doc/vim.md#neovim
 local lspconfig = require("lspconfig")
+
+-- Should work so long as `gopls` command is on $PATH
 lspconfig.gopls.setup({})
 
 -- lsp configuration suggestions can be found here:
