daa49732b7fb4bf505ebd846e536cae48aa79dce
Ethan Estrada <ethan@misterfidget.com>
2023-12-19 16:14:50 -0600

    Create bootstrap util for downloading files

--- .config/nvim/lua/bootstrap/plug.lua
+++ .config/nvim/lua/bootstrap/plug.lua
@@ -1,34 +1,24 @@
-local ensure_vim_plug = function()
-  local fn = vim.fn
+local utils = require('bootstrap.utils')
 
-  local install_path = fn.stdpath('data') .. '/site/autoload/plug.vim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    local vim_plug_url = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    fn.system({ 'curl', '-fLo', install_path, '--create-dirs', vim_plug_url })
-    -- Source it explicitly this first time. Will be autoloaded by neovim on
-    -- future startups.
-    vim.cmd('source ' .. install_path)
-    return true
-  else
-    return false
-  end
-end
+local M = {}
 
-local plug_module = {}
-plug_module.Bootstrapped = ensure_vim_plug()
+M.Bootstrapped = utils.download_file(
+  vim.fn.stdpath('data') .. '/site/autoload/plug.vim',
+  'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+)
 
 -- For scripting
-plug_module.Plug = vim.fn['plug#']
-plug_module.Begin = vim.fn['plug#begin']
-plug_module.End = vim.fn['plug#end']
+M.Plug = vim.fn['plug#']
+M.Begin = vim.fn['plug#begin']
+M.End = vim.fn['plug#end']
 
 -- Mostly interactive commands that *can* be useful in scripting
-plug_module.Clean = function() vim.cmd(':PlugClean') end
-plug_module.Diff = function() vim.cmd(':PlugDiff') end
-plug_module.Install = function() vim.cmd(':PlugInstall') end
-plug_module.Snapshot = function() vim.cmd(':PlugSnapshot') end
-plug_module.Status = function() vim.cmd(':PlugStatus') end
-plug_module.Update = function() vim.cmd(':PlugUpdate') end
-plug_module.Upgrade = function() vim.cmd(':PlugUpgrade') end
+M.Clean = function() vim.cmd(':PlugClean') end
+M.Diff = function() vim.cmd(':PlugDiff') end
+M.Install = function() vim.cmd(':PlugInstall') end
+M.Snapshot = function() vim.cmd(':PlugSnapshot') end
+M.Status = function() vim.cmd(':PlugStatus') end
+M.Update = function() vim.cmd(':PlugUpdate') end
+M.Upgrade = function() vim.cmd(':PlugUpgrade') end
 
-return plug_module
+return M
---
+++ .config/nvim/lua/bootstrap/utils.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+-- Return `true` if file is downloaded, otherwise false
+M.download_file = function(install_path, download_url)
+  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    vim.fn.system({ 'curl', '-fLo', install_path, '--create-dirs', download_url })
+    return true
+  else
+    return false
+  end
+end
+
+return M
