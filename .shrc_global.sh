bf70ddbe5c3bc5007cadafcadc9fc8bed917085e
Ethan Estrada <ethan@misterfidget.com>
2024-01-28 14:39:52 -0600

    Move persicope to correct place to be loaded

--- .shrc_global.sh
+++ .shrc_global.sh
@@ -390,7 +390,7 @@ case `id -u` in
     *) PS1="${PS1}$ ";;
 esac
 
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
