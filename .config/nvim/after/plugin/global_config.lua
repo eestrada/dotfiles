ec025fa144172e4e1688a45aceacdbc5e90cf26c
Ethan Estrada <ethan@misterfidget.com>
2024-10-04 17:12:09 -0500

    Don't lint when entering buffer
    This had some odd side effects,
    like linting floating hint windows when entering them.
    
    Besides, I only care to lint when I am editing a file,
    so linting can wait until after leaving Insert mode or saving the buffer.

--- .config/nvim/after/plugin/global_config.lua
+++ .config/nvim/after/plugin/global_config.lua
@@ -420,7 +420,7 @@ local function nvim_lint_setup()
 
   local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
-  vim.api.nvim_create_autocmd({ 'InsertLeave', 'BufEnter', 'BufWritePost' }, {
+  vim.api.nvim_create_autocmd({ 'InsertLeave', 'BufWritePost' }, {
     group = lint_augroup,
     callback = function()
       lint.try_lint()
