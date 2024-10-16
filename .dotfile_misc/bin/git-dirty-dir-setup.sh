e9fef43724909dbb925ffc75e5291788ba6d4979
Ethan Estrada <ethan@misterfidget.com>
2024-10-15 22:28:40 -0500

    Add argument support for specifying branch

--- .dotfile_misc/bin/git-dirty-dir-setup.sh
+++ .dotfile_misc/bin/git-dirty-dir-setup.sh
@@ -1,24 +1,40 @@
 #!/bin/sh
+
+# Simple script to clone a git repo
+# into a directory with pre-existing files in it.
+#
+# This script does not overwrite existing files.
+# If you want to overwrite existing files
+# without first diffing them against the state of the repo,
+# all you need to run is: `git restore .` or `git checkout .`
+# after running this script.
 set -e
 
 _default_repo='git@github.com:eestrada/dotfiles.git'
+_default_branch='master'
+_default_dir="${HOME}"
 REPO_URL=${1:-${_default_repo}}
-DIRECTORY=${2:-${HOME}}
+BRANCH=${2:-${_default_branch}}
+DIRECTORY=${3:-${_default_dir}}
 unset _default_repo
+unset _default_branch
+unset _default_dir
 
 # echo "${REPO_URL}"
+# echo "${BRANCH}"
 # echo "${DIRECTORY}"
+# exit
 
 cd "${DIRECTORY}" || exit
 
 # start as empty repo
 git init
 
-# git remote add --fetch --track=master origin git@github.com:eestrada/dotfiles.git
-git remote add --fetch --track=master origin "${REPO_URL}"
+# set up remote, but don't check it out
+git remote add --fetch "--track=${BRANCH}" origin "${REPO_URL}"
 
 # Force repo to think it is checked out to the same commit as the `origin` remote
-cat .git/refs/remotes/origin/master >.git/refs/heads/master
+cat ".git/refs/remotes/origin/${BRANCH}" >".git/refs/heads/${BRANCH}"
 
 # We haven't actually checked out, so we need to make the index reflect reality.
 git restore --staged .
