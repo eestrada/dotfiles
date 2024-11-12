0a05536cdb4a52d18842cf0a3f7fef4e109e9f76
Ethan Estrada <ethan@misterfidget.com>
2024-11-11 18:24:47 -0600

    Force only one sign per line for diagnostics signs handler

--- .config/nvim/after/plugin/global_config.lua
+++ .config/nvim/after/plugin/global_config.lua
@@ -1189,6 +1189,42 @@ local function general_neovim_setup()
   }
 
   vim.diagnostic.config(dconfig)
+
+  -- Code below taken from Nvim diagnostics help page.
+
+  -- Create a custom namespace. This will aggregate signs from all other
+  -- namespaces and only show the one with the highest severity on a
+  -- given line
+  local ns = vim.api.nvim_create_namespace('my_namespace')
+
+  -- Get a reference to the original signs handler
+  local orig_signs_handler = vim.diagnostic.handlers.signs
+
+  -- Override the built-in signs handler
+  vim.diagnostic.handlers.signs = {
+    show = function(_, bufnr, _, opts)
+      -- Get all diagnostics from the whole buffer rather than just the
+      -- diagnostics passed to the handler
+      local diagnostics = vim.diagnostic.get(bufnr)
+
+      -- Find the "worst" diagnostic per line
+      local max_severity_per_line = {}
+      for _, d in pairs(diagnostics) do
+        local m = max_severity_per_line[d.lnum]
+        if not m or d.severity < m.severity then
+          max_severity_per_line[d.lnum] = d
+        end
+      end
+
+      -- Pass the filtered diagnostics (with our custom namespace) to
+      -- the original handler
+      local filtered_diagnostics = vim.tbl_values(max_severity_per_line)
+      orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
+    end,
+    hide = function(_, bufnr)
+      orig_signs_handler.hide(ns, bufnr)
+    end,
+  }
 end
 
 -- [[ Run all setup functions ]] {{{1
