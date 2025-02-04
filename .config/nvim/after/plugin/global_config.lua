96267c68520b880a02d4c063f5913683189692f0
Ethan Estrada <ethan@misterfidget.com>
2025-02-04 15:28:00 -0600

    Modify Gen settings

--- .config/nvim/after/plugin/global_config.lua
+++ .config/nvim/after/plugin/global_config.lua
@@ -1153,11 +1153,11 @@ local function llm_setup()
     accept_map = '<c-cr>',  -- set keymap to replace the previous selection with the last result
     host = 'localhost',     -- The host running the Ollama service.
     port = '11434',         -- The port on which the Ollama service is listening.
-    display_mode = 'float', -- The display mode. Can be "float" or "split" or "horizontal-split".
-    show_prompt = false,    -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
-    show_model = false,     -- Displays which model you are using at the beginning of your chat session.
+    display_mode = 'split', -- The display mode. Can be "float" or "split" or "horizontal-split".
+    show_prompt = true,     -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
+    show_model = true,      -- Displays which model you are using at the beginning of your chat session.
     no_auto_close = false,  -- Never closes the window automatically.
-    file = false,           -- Write the payload to a temporary file to keep the command short.
+    file = true,            -- Write the payload to a temporary file to keep the command short.
     hidden = false,         -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
     init = function(options)
       pcall(io.popen, 'ollama serve > /dev/null 2>&1 &')
